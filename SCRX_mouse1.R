7684ef065c2ad094d4b14b917e5607b114150dda
Julian Rees <julianrees@users.noreply.github.com>
2017-03-02 16:33:52 -0800

    ignoring new folder Rfigs, SCRX_mouse1 now autosaves 17 plots to the Rfigs folder

--- SCRX_mouse1.R
+++ SCRX_mouse1.R
@@ -8,9 +8,9 @@ library(plyr)
 # import the data
 mice <- read.xlsx("../Data/SCRXmouse1Weights.xlsx", header = TRUE, sheetIndex = 1)
 
-# convert row names to mouse number and drop mouse number column
+# convert row names to mouse number and convert mouse number column to factor
 rownames(mice) <- mice$Mouse..
-mice <- mice[, -2]
+mice$Mouse.. <- as.factor(rownames(mice))
 
 # calculate the doses that each mouse received based on the dosing sheet
 day0dose <- cut(mice$Day.0, breaks = 15.8+1.5*0:7, 
@@ -20,227 +20,234 @@ day1dose <- cut(mice$Day.1, breaks = 15.8+1.5*0:7,
 mice_dose <- cbind(mice[,1:3], day0dose, day1dose)
 
 # modify col names for day to be numeric
-colnames(mice) <- c('group', '0', '1', '4', '7', '11')
+colnames(mice) <- c('group', 'mousenum', 
+                    unlist(strsplit(colnames(mice)[3:ncol(mice)], '.', fixed = TRUE))[1:(ncol(mice)-2)*2])
 
 # create factor for dose radionuclide
 agent <- cut(as.numeric(mice$group), breaks = c(0,5,9,10,11),
              labels = c('Ac225','Lu177','Chelator','Sentinal'))
 mice <- cbind(mice, agent)
 
-# melt the data to plot 
-mmice <- melt(mice, id = c("group", "agent"))
+# compute the differences from day 0
+dmice0 <- mice
+refday <- 0
+for(i in seq(ncol(dmice0)-3)){
+  dmice0[,i+2] = dmice0[,i+2] - mice[,3+refday]
+}
 
-fig_weight <- ggplot(mmice, aes(x = variable, y = value, by = group)) + 
-  geom_boxplot(aes(color = group)) + guides(color=FALSE) +
+# compute the differences from day 1
+dmice1 <- mice
+refday <- 1
+for(i in seq(ncol(dmice1)-3)){
+  dmice1[,i+2] = dmice1[,i+2] - mice[,3+refday]
+}
+
+# trim day 0 data
+dmice1$`0` <- NULL
+
+#---------- END OF DATA MANIPULATION !-!-! PLOTTING STARTS ----------------
+w = 0.65
+fwid = 9
+fhei = 6
+
+
+#------ Plots of weight vs group by day -------
+
+dat <- melt(mice, id = c("group", "agent", "mousenum"))
+ggplot(dat, aes(x = group, y = value, by = variable)) + 
+  geom_boxplot(aes(color = variable), width = w) + guides(color=FALSE) + 
+  geom_boxplot(aes(fill = variable), width = w, outlier.color = NA) + 
+  theme_bw() +
+  scale_x_discrete() +
+  labs(x = "Group", y = "Weight (g)", fill = "Day") + 
+  ggsave(filename = 'Rfigs/weight_byDay.png', 
+         width = fwid, height = fhei, units = "in")
+
+dat <- melt(dcast(dat, group ~ variable, mean), id = 'group')
+ggplot(dat, aes(x = group, y = value, by = variable)) + 
+  geom_bar(aes(fill = variable), position = 'dodge', stat = 'identity', width = w) + 
+  theme_bw() +
+  scale_x_discrete() +
+  labs(x = "Group", y = "Weight (g)", fill = "Day") + 
+  ggsave(filename = 'Rfigs/weight_byDay_mean.png', 
+         width = fwid, height = fhei, units = "in")
+
+dat <- melt(dmice0, id = c("group", "agent", "mousenum"))
+ggplot(dat, aes(x = group, y = value, by = variable)) + 
+  geom_boxplot(aes(color = variable), width = w) + guides(color=FALSE) + 
+  geom_boxplot(aes(fill = variable), width = w, outlier.color = NA) + 
+  theme_bw() +
+  scale_x_discrete() +
+  labs(x = "Group", y = "Weight Change from Day 0 (g)", fill = "Day") + 
+  ggsave(filename = 'Rfigs/weightDiff0_byDay.png', 
+         width = fwid, height = fhei, units = "in")
+
+dat <- melt(dcast(dat, group ~ variable, mean), id = 'group')
+ggplot(dat, aes(x = group, y = value, by = variable)) + 
+  geom_bar(aes(fill = variable), position = 'dodge', stat = 'identity', width = w) + 
+  theme_bw() +
+  scale_x_discrete() +
+  labs(x = "Group", y = "Weight Change from Day 0 (g)", fill = "Day") + 
+  ggsave(filename = 'Rfigs/weightDiff0_byDay_mean.png', 
+         width = fwid, height = fhei, units = "in")
+
+
+dat <- melt(dmice1, id = c("group", "agent", "mousenum"))
+ggplot(dat, aes(x = group, y = value, by = variable)) + 
+  geom_boxplot(aes(color = variable), width = w) + guides(color=FALSE) + 
+  geom_boxplot(aes(fill = variable), width = w, outlier.color = NA) + 
+  theme_bw() +
+  scale_x_discrete() +
+  labs(x = "Group", y = "Weight Change from Day 1 (g)", fill = "Day") + 
+  ggsave(filename = 'Rfigs/weightDiff1_byDay.png', 
+         width = fwid, height = fhei, units = "in")
+
+dat <- melt(dcast(dat, group ~ variable, mean), id = 'group')
+ggplot(dat, aes(x = group, y = value, by = variable)) + 
+  geom_bar(aes(fill = variable), position = 'dodge', stat = 'identity', width = w) + 
+  theme_bw() +
+  scale_x_discrete() +
+  labs(x = "Group", y = "Weight Change from Day 1 (g)", fill = "Day") + 
+  ggsave(filename = 'Rfigs/weightDiff1_byDay_mean.png', 
+         width = fwid, height = fhei, units = "in")
+
+
+#------- Plots of Weight vs Day by group ----------
+
+dat <- melt(mice, id = c("group", "agent", "mousenum"))
+ggplot(dat, aes(x = variable, y = value, by = group)) + 
+  geom_boxplot(aes(fill = group, color = group), outlier.color = NULL) + guides(color=FALSE) +
   geom_boxplot(aes(fill = group), outlier.color = NA) + 
-  #geom_line(aes(x = as.numeric(as.character(variable)), color = group)) +
   theme_bw() +
   labs(x = "Day", y = "Weight", fill = "Group") +
-  scale_x_discrete(limits=1:12) +
-  facet_wrap(~agent)
-fig_weight
+  #scale_x_discrete(limits=1:11) +
+  facet_wrap(~agent) + 
+  ggsave(filename = 'Rfigs/weight_byGroup.png', 
+         width = fwid, height = fhei, units = "in")
 
-
-# compute the differences based on ref day 0
-dmice <- mice
-refday <- 0
-
-for(i in seq(ncol(dmice)-2)){
-  dmice[,i+1] = dmice[,i+1] - mice[,2+refday]
-}
-mdmice <- melt(dmice, id = c("group", "agent"))
-
-fig_day <- ggplot(mdmice, aes(x = group, y = value, by = variable)) + 
-  geom_boxplot(aes(color = variable), position = 'dodge') + 
-  geom_bar(aes(fill = variable), position = 'dodge', stat = 'identity') +
-  theme_bw() +
-  labs(x = "Group", y = "Weight Change (Day 0)", fill = "Group", color = NULL)
-
-fig_day
-
-update_geom_defaults("point", list(color = NULL))
-fig_day <- ggplot(mdmice, aes(x = group, y = value, by = variable)) + 
-  geom_boxplot(aes(color = variable), position = 'dodge') + guides(color = FALSE) +
-  geom_boxplot(aes(fill = variable), position = 'dodge', outlier.color = NA) + 
-  theme_bw() +
-  labs(x = "Group", y = "Weight Change (Day 0)", fill = "Group", color = NULL)
-
-fig_day
-
-
-
-
-
-
-
-
-
-
-
-
-# compute the differences based on ref day 1
-dmice <- mice
-refday <- 1
-
-for(i in seq(ncol(dmice)-2)){
-  dmice[,i+1] = dmice[,i+1] - mice[,2+refday]
-}
-mdmice <- melt(dmice, id = c("group", "agent"))
-
-
-
-update_geom_defaults("point", list(color = NULL))
-fig_day <- ggplot(dat, aes(x = group, y = value, by = variable)) + 
-  geom_boxplot(aes(color = variable), position = 'dodge') +
-  guides(color = FALSE) +
-  geom_boxplot(aes(fill = variable), position = 'dodge', outlier.color = NA) + 
-  theme_bw() +
-  labs(x = "Group", y = "Weight Change (Day 1)", fill = "Group", color = NULL)
-
-fig_day
-
-
-
-dat <- mdmice[ which(mdmice$variable != 0), ]
-
-update_geom_defaults("point", list(color = NULL))
-fig_day <- ggplot(dat, aes(x = group, y = value, by = variable)) + 
-  geom_boxplot(aes(color = variable), position = 'dodge') +
-  guides(color = FALSE) +
-  geom_boxplot(aes(fill = variable), position = 'dodge', outlier.color = NA) + 
-  theme_bw() +
-  labs(x = "Group", y = "Weight Change (Day 1)", fill = "Group", color = NULL)
-
-fig_day
-
-
-
-
-
-fig_weight_diff <- ggplot(mdmice, aes(x = variable, y = value, by = group)) + 
-  geom_boxplot(aes(fill = group, color = group), outlier.color = NULL) + 
+dat <- melt(dmice0, id = c("group", "agent", "mousenum"))
+ggplot(dat, aes(x = variable, y = value, by = group)) + 
+  geom_boxplot(aes(fill = group, color = group), outlier.color = NULL) + guides(color=FALSE) +
   geom_boxplot(aes(fill = group), outlier.color = NA) + 
-  #geom_line(aes(x = as.numeric(as.character(variable)), color = group, group = group)) +
   theme_bw() +
-  labs(x = "Day", y = "Weight Change", fill = "Group") +
-  scale_x_discrete(limits=1:11) +
-  facet_wrap(~agent)
-fig_weight_diff
+  labs(x = "Day", y = "Weight", fill = "Group") +
+  #scale_x_discrete(limits=1:11) +
+  facet_wrap(~agent) + 
+  ggsave(filename = 'Rfigs/weightDiff0_byGroup.png', 
+         width = fwid, height = fhei, units = "in")
 
-
-fig_weight_diff <- ggplot(mdmice, aes(x = variable, y = value, by = group)) + 
-  geom_boxplot(aes(fill = group), outlier.color = NULL) + 
-  #geom_line(aes(x = as.numeric(as.character(variable)), color = group, group = group)) +
+dat <- melt(dmice1, id = c("group", "agent", "mousenum"))
+ggplot(dat, aes(x = variable, y = value, by = group)) + 
+  geom_boxplot(aes(fill = group, color = group), outlier.color = NULL) + guides(color=FALSE) +
+  geom_boxplot(aes(fill = group), outlier.color = NA) + 
   theme_bw() +
-  labs(x = "Day", y = "Weight Change", fill = "Group") +
-  scale_x_discrete(limits=1:11) +
-  facet_wrap(~group)
-fig_weight_diff
+  labs(x = "Day", y = "Weight", fill = "Group") +
+  #scale_x_discrete(limits=1:11) +
+  facet_wrap(~agent) + 
+  ggsave(filename = 'Rfigs/weightDiff1_byGroup.png', 
+         width = fwid, height = fhei, units = "in")
 
+#---------- Plots of Weight vs Day by Group - per Agent ------------
 
-dat <- mdmice [which(mdmice$agent == 'Ac225' | mdmice$agent == 'Sentinal'), ]
-
-fig_weight_diff <- ggplot(dat, aes(x = variable, y = value, by = group)) + 
-  geom_boxplot(aes(fill = group), outlier.color = NULL) + 
-  #geom_line(aes(x = as.numeric(as.character(variable)), color = group, group = group)) +
+dat <- melt(dmice0[ which(dmice0$agent == 'Ac225' | dmice0$agent == 'Sentinal' | dmice0$agent == 'Chelator'), ],
+            id = c("group", "agent", "mousenum"))
+ggplot(dat, aes(x = variable, y = value, by = group)) + 
+  geom_boxplot(aes(fill = group, color = group), outlier.color = NULL) + guides(color=FALSE) +
+  geom_boxplot(aes(fill = group), outlier.color = NA) + 
   theme_bw() +
-  labs(x = "Day", y = "Weight Change", fill = "Group") +
-  scale_x_discrete(limits=1:11) +
-  facet_wrap(~group)
-fig_weight_diff
+  labs(x = "Day", y = "Weight Change from Day 0 (g)", fill = "Group") +
+  #scale_x_discrete(limits=1:11) +
+  facet_wrap(~group, nrow=1) + 
+  ggsave(filename = 'Rfigs/weightDiff0_byGroup_Ac225.png', 
+         width = fwid, height = fhei, units = "in")
 
+dat <- melt(dmice0[ which(dmice0$agent == 'Lu177' | dmice0$agent == 'Sentinal' | dmice0$agent == 'Chelator'), ],
+            id = c("group", "agent", "mousenum"))
+ggplot(dat, aes(x = variable, y = value, by = group)) + 
+  geom_boxplot(aes(fill = group, color = group), outlier.color = NULL) + guides(color=FALSE) +
+  geom_boxplot(aes(fill = group), outlier.color = NA) + 
+  theme_bw() +
+  labs(x = "Day", y = "Weight Change from Day 0 (g)", fill = "Group") +
+  #scale_x_discrete(limits=1:11) +
+  facet_wrap(~group, nrow=1) + 
+  ggsave(filename = 'Rfigs/weightDiff0_byGroup_Lu177.png', 
+         width = fwid, height = fhei, units = "in")
 
- # add the mouse number back to data
+dat <- melt(dmice1[ which(dmice1$agent == 'Ac225' | dmice1$agent == 'Sentinal' | dmice1$agent == 'Chelator'), ],
+            id = c("group", "agent", "mousenum"))
+ggplot(dat, aes(x = variable, y = value, by = group)) + 
+  geom_boxplot(aes(fill = group, color = group), outlier.color = NULL) + guides(color=FALSE) +
+  geom_boxplot(aes(fill = group), outlier.color = NA) + 
+  theme_bw() +
+  labs(x = "Day", y = "Weight Change from Day 1 (g)", fill = "Group") +
+  #scale_x_discrete(limits=1:11) +
+  facet_wrap(~group, nrow=1) + 
+  ggsave(filename = 'Rfigs/weightDiff1_byGroup_Ac225.png', 
+         width = fwid, height = fhei, units = "in")
 
-micenum <- cbind(dmice, rownames(dmice))
-colnames(micenum) <- c(colnames(micenum)[-ncol(micenum)], 'mousenumber')
-mdmicenum <- melt(micenum, id = c("group", "agent", "mousenumber"))
+dat <- melt(dmice1[ which(dmice1$agent == 'Lu177' | dmice1$agent == 'Sentinal' | dmice1$agent == 'Chelator'), ],
+            id = c("group", "agent", "mousenum"))
+ggplot(dat, aes(x = variable, y = value, by = group)) + 
+  geom_boxplot(aes(fill = group, color = group), outlier.color = NULL) + guides(color=FALSE) +
+  geom_boxplot(aes(fill = group), outlier.color = NA) + 
+  theme_bw() +
+  labs(x = "Day", y = "Weight Change from Day 1 (g)", fill = "Group") +
+  #scale_x_discrete(limits=1:11) +
+  facet_wrap(~group, nrow=1) +
+  ggsave(filename = 'Rfigs/weightDiff1_byGroup_Lu177.png', 
+         width = fwid, height = fhei, units = "in")
 
+#---------- Plots of Weight Change vs Day by Group - per Agent - Individual Mice ------------
 
-
-dat <- mdmicenum[ which(mdmicenum$agent == 'Ac225' & mdmicenum$variable != 0), ]
-dat <- mdmicenum[ which(mdmicenum$variable != 0), ]
+dat <- melt(dmice0[ which(dmice0$agent == 'Ac225' | dmice0$agent == 'Sentinal' | dmice0$agent == 'Chelator'), ],
+            id = c("group", "agent", "mousenum"))
 dat$variable <- as.numeric(levels(dat$variable)[dat$variable])
-
-ggplot(dat, aes(x = variable, y = value, by = mousenumber)) + 
+ggplot(dat, aes(x = variable, y = value, by = mousenum)) + 
   geom_line(aes(color = group)) + 
   geom_point(aes(color = group)) +
   theme_bw() +
-  labs(x = "Day", y = "Weight Change", fill = "Group") +
-  scale_x_discrete(limits=1:11) +
-  facet_wrap(~group)
+  labs(x = "Day", y = "Weight Change from Day 0 (g)", color = "Group") +
+  #scale_x_discrete(limits=1:11) +
+  facet_wrap(~group, nrow=1) + 
+  ggsave(filename = 'Rfigs/weightDiff0_byGroup_Ac225_indivMice.png', 
+         width = fwid, height = fhei, units = "in")
 
-
-
-
-fig_weight_diff <- ggplot(mdmicenum, aes(x = variable, y = value, by = mousenumber)) + 
+dat <- melt(dmice0[ which(dmice0$agent == 'Lu177' | dmice0$agent == 'Sentinal' | dmice0$agent == 'Chelator'), ],
+            id = c("group", "agent", "mousenum"))
+dat$variable <- as.numeric(levels(dat$variable)[dat$variable])
+ggplot(dat, aes(x = variable, y = value, by = mousenum)) + 
   geom_line(aes(color = group)) + 
-  #geom_line(aes(x = as.numeric(as.character(variable)), color = group, group = group)) +
+  geom_point(aes(color = group)) +
   theme_bw() +
-  labs(x = "Day", y = "Weight Change", fill = "Group") +
-  scale_x_discrete(limits=1:11) +
-  facet_wrap(~agent)
-fig_weight_diff
+  labs(x = "Day", y = "Weight Change from Day 0 (g)", color = "Group") +
+  #scale_x_discrete(limits=1:11) +
+  facet_wrap(~group, nrow=1) +
+  ggsave(filename = 'Rfigs/weightDiff0_byGroup_Lu177_indivMice.png', 
+         width = fwid, height = fhei, units = "in")
 
+dat <- melt(dmice1[ which(dmice1$agent == 'Ac225' | dmice1$agent == 'Sentinal' | dmice1$agent == 'Chelator'), ],
+            id = c("group", "agent", "mousenum"))
+dat$variable <- as.numeric(levels(dat$variable)[dat$variable])
+ggplot(dat, aes(x = variable, y = value, by = mousenum)) + 
+  geom_line(aes(color = group)) + 
+  geom_point(aes(color = group)) +
+  theme_bw() +
+  labs(x = "Day", y = "Weight Change from Day 1 (g)", color = "Group") +
+  #scale_x_discrete(limits=1:11) +
+  facet_wrap(~group, nrow=1) +
+  ggsave(filename = 'Rfigs/weightDiff1_byGroup_Ac225_indivMice.png', 
+         width = fwid, height = fhei, units = "in")
 
+dat <- melt(dmice1[ which(dmice1$agent == 'Lu177' | dmice1$agent == 'Sentinal' | dmice1$agent == 'Chelator'), ],
+            id = c("group", "agent", "mousenum"))
+dat$variable <- as.numeric(levels(dat$variable)[dat$variable])
+ggplot(dat, aes(x = variable, y = value, by = mousenum)) + 
+  geom_line(aes(color = group)) + 
+  geom_point(aes(color = group)) +
+  theme_bw() +
+  labs(x = "Day", y = "Weight Change from Day 1 (g)", color = "Group") +
+  #scale_x_discrete(limits=1:11) +
+  facet_wrap(~group, nrow=1) +
+  ggsave(filename = 'Rfigs/weightDiff1_byGroup_Lu177_indivMice.png', 
+         width = fwid, height = fhei, units = "in")
 
-
-
-# count up the NA values (dead mice)
-# 
-# survivingmice2 <- ddply(mmice, .(group, variable, agent), summarize, 
-#       alive = is.finite(value))
-# survivingmice2 <- subset(survivingmice2, alive == TRUE)
-# 
-# fig_alive_count <- ggplot(survivingmice2, aes(x = variable, 
-#                                              by = group)) + 
-#   geom_bar(aes(fill = group), position = position_dodge(width = 1.1)) + 
-#   labs(x = "Day", y = "Surviving Mice", fill = "Group") +
-#   theme_bw() +
-#   scale_x_discrete(limits=1:6) +
-#   facet_wrap(~agent)
-# fig_alive_count
-
-
-# STOP HERE FOR COPYING TO REPORT - ALTERNATE SURVIVAL FIGURES BELOW
-
-# survivingmice <- ddply(mmice, .(group, variable, agent), summarize,
-#                        dead = sum(is.na(value)), 
-#                        live = 5 - dead)
-# 
-# # this gives large filled in areas
-# fig_dead_count <- ggplot(survivingmice, aes(x = as.numeric(as.character(variable)), 
-#                                             y = live, by = group)) + 
-#   geom_area(aes(fill = group), position = position_dodge(width = 0.2), alpha = 0.2) + 
-#   labs(x = "Day", y = "Survival (%)", color = "Group") +
-#   theme_bw() +
-#   scale_x_discrete(limits=1:6) +
-#   facet_wrap(~agent)
-# fig_dead_count
-# 
-# # this gives just vertical bars
-# fig_dead_count <- ggplot(survivingmice, aes(x = variable, 
-#                                             y = live, by = group)) + 
-#   geom_area(aes(color = group), position = position_dodge(width = 0.2)) + 
-#   labs(x = "Day", y = "Survival (%)", color = "Group") +
-#   theme_bw() +
-#   scale_x_discrete(limits=1:6) +
-#   facet_wrap(~agent)
-# fig_dead_count
-# 
-# 
-# 
-# # working on improving vertical bars
-# 
-# 
-# # this is survival tracked with lines and area fill
-# fig_survival <- ggplot(survivingmice, aes(x = as.numeric(as.character(variable)), 
-#                                           y = live*20, by = group)) + 
-#   geom_line(aes(color = group)) + 
-#   geom_point(aes(color = group)) + 
-#   geom_area(aes(fill = group), position = position_dodge(width = 0.2), alpha = 0.1) + 
-#   labs(x = "Day", y = "Survival (%)", color = "Group") +
-#   scale_y_continuous(breaks = seq(0,100,20), limits = c(0,100)) +
-#   theme_bw() +
-#   facet_wrap(~agent)
-# fig_survival
